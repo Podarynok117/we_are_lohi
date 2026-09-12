e4cbb51e052ed8454c550002f346cbf64ef4183b
arnuhcg <oleksandrevlah7@gmail.com>
2026-09-12 13:32:23 +0300

    wtf is goin on

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 let socket=io();
 let input = document.getElementById("message");
 let button = document.getElementById("send");
+let messages = document.getElementById("messages");
 
 function showMessage(message) {
     console.log(message);
@@ -10,5 +11,8 @@ socket.on("message",showMessage);
 
 button.onclick = function() {
     socket.emit("message", input.value);
+    let p=document.createElement("p")
+    p.textContent=input.value
+    messages.appendChild(p);
     input.value = "";
 };
